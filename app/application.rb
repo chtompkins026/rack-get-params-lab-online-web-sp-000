3012f1a227bc801b7f629311999683237cefd29b
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-23 16:34:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -25,7 +25,7 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       if @@items.include?(search_term)
-        @@carts.push(search_term)
+        @@cart.push(search_term)
       else 
         resp.write "We don't have that item"
       end 
