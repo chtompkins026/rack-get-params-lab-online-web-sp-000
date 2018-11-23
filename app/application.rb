58858920547a98e583c1dd1c657d087e785b5734
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-23 16:25:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+      puts "Your cart is empty" if @@cart.empty? 
       
     else
       resp.write "Path Not Found"
